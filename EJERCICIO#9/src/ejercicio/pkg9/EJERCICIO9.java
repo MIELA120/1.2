a572eb186fe43a6dac2360ae2152bd2904a6c762
MIELA120 <81060044+MIELA120@users.noreply.github.com>
2021-03-21 12:03:29 -0500

    Add files via upload

---
+++ EJERCICIO#9/src/ejercicio/pkg9/EJERCICIO9.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.pkg9;
+
+  import  javax.swing.JOptionPane;
+/**
+ *
+ * @author MILENA HERNANDEZ
+ */
+public class EJERCICIO9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     String n="";
+        int numA=0,numB=0;
+        int divi1=0,divi2 = 0;
+        int primos,entero,a,b;
+       
+        entero = Integer.parseInt(JOptionPane.showInputDialog(" digite numero 1"));
+        entero = Integer.parseInt(JOptionPane.showInputDialog(" digite numero 2"));
+        
+        for(int i=1;i<=numA/2;i++){
+         
+            if(numA/1==0)
+                divi1+=1;
+          
+                       
+    }
+        for(int i=1;i<=numB/2;i++){
+            if(numB/i==0)
+                divi2+=1;
+        }
+       
+            if(numA==divi2&&numB==divi1)
+             
+         JOptionPane.showMessageDialog(null," son numeros primos"+"\n"+n);
+            else 
+                
+    
+      JOptionPane.showMessageDialog(null,"no son numeros primos"+"\n"+n);
+
+    }
+}
+    
+    
+        
+        
+
+
